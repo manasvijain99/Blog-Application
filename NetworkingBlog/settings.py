c1d802e58852f9ed629b77f7f0f27d814f3e6c15
Zomentum-Ticket-Booking-System <manasvijain99>
2020-10-07 23:57:01 +0530

    first commit

--- NetworkingBlog/settings.py
+++ NetworkingBlog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7w9du1l7p86svzn4lf#0h)lw6^c-=-vgttt7=2(nbw9wir0oey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
